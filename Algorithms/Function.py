a85e34b9d470525ec32223788ba8af0c25e90455
María Alejandra Hernández <alejahernandezrojas@hotmail.com>
2021-11-08 16:29:38 -0500

    update DCT2D doc string

--- Algorithms/Function.py
+++ Algorithms/Function.py
@@ -6,6 +6,10 @@ from scipy.sparse import csr_matrix
 import inspect
 
 def dct2():
+    '''
+    This is a Discrete Cosine Transform for 2D signals
+    (completar con una descripcion detallada en ingles)
+    '''
     def dct2_function(x):
         return scipy.fft.dct(scipy.fft.dct(x.T).T)
     return  dct2_function
