943bdd59da77a843394a7e44fedcc9de4290515c
kareth <kareth.marcela@gmail.com>
2021-11-15 02:32:51 -0500

    Update of the main function and mainly of the H operator and other functions that have to estimate the removed columns from the shot.

--- Algorithms/Function.py
+++ Algorithms/Function.py
@@ -7,26 +7,48 @@ import inspect
 import scipy.sparse.linalg as ln
 from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral,
                                  denoise_wavelet, estimate_sigma)
-import time
-# from skimage.measure import (compare_psnr, compare_ssim)
 
+def random_sampling(x,sr):
+  ''' Random Sampling
+  x : full data
+  sr: subsampling factor
+  '''
+  dim = x.shape
+  batch = 1 #dim[0]
+  M  = dim[0]
+  N  = dim[1]
+  # L = dim[3]
+
+  tasa_compression = int(sr*N)
+  pattern_vec = np.ones((N,))
+
+  ss = np.random.permutation(list(range(1, N-1)))
+  pattern_vec[ss[0:tasa_compression]] = 0
+  H0 = np.tile(pattern_vec.reshape(1,-1),(M,1))
+
+  out = x * H0
+
+  pattern_bool = np.asarray(pattern_vec, dtype=bool)
+
+
+  return out,pattern_vec, pattern_bool
 
 def dct2():
     '''
     This is a Discrete Cosine Transform for 2D signals
-    (completar con una descripcion detallada en ingles)
     '''
     def dct2_function(x):
-        return scipy.fft.dct(scipy.fft.dct(x.T).T)
+        return (scipy.fft.dct(scipy.fft.dct(x).T)).T
     return  dct2_function
 
 def idct2():
+    ''' Inverse operator of the 2D discrete cosine transform'''
     def idct2_function(x):
-        return scipy.fft.idct(scipy.fft.idct(x.T).T)
+        return (scipy.fft.idct(scipy.fft.idct(x).T)).T
     return idct2_function
 
 class Operator:
-  def __init__(self, H, m,n,operator_dir,operator_inv):
+  def __init__(self, H, m, n, operator_dir,operator_inv):
     self.H = H
     self.m = m
     self.n = n
@@ -34,19 +56,23 @@ class Operator:
     self.operator_inv = operator_inv
 
   def transpose(self,x):  # y = D'H' * x
-      y = self.H.T * np.squeeze(x.reshape(-1))  # H' * x
-      y = np.reshape(y, [self.m, self.n])
+      Ht = self.H.transpose()
+      y = Ht* np.squeeze(x.T.reshape(-1))  # H' * x
+
+      y = np.reshape(y, [self.m, self.n],order='F')
+
       y = self.operator_dir(y)
+
       return  y
 
   def direct(self,x): # y = H * D * x
-      x = np.reshape(x, [self.m, self.n])  # ordenar
+      x = np.reshape(x, [self.m, self.n],order='F')  # ordenar
+
       theta = self.operator_inv(x)  # D * x
-      y = self.H * np.squeeze(theta.reshape(-1))  # H * D * x
+
+      y = self.H * np.squeeze(theta.T.reshape(-1))  # H * D * x
+
       return  y
-
-
-
 
 # -------------------------------------------------------------------------
 def soft_threshold(x,t):
@@ -70,27 +96,41 @@ class Algorithms:
 
         # ------- change the dimension of the inputs image --------
         m, n = x.shape
-        m = int(2 ** (np.ceil(np.log2(m)) - 1))
-        n = int(2 ** (np.ceil(np.log2(n)) - 1))
+        #m = int(2 ** (np.ceil(np.log2(m)) - 1))
+        #n = int(2 ** (np.ceil(np.log2(n)) - 1))
         x = transform.resize(x, (m, n))
         x = x / np.abs(x).max()
         self.x = x
         self.m, self.n = x.shape
+        self.pattern = 0
 
         # ---------- Load or build the sensing matrix -------------
-        if (np.sum(H) > 1):
-            self.H = H
+        if isinstance(H, (list, tuple, np.ndarray)):
+            if (len(H.shape) == 1):
+                # Create H from a given input pattern
+                # The input H must be the pattern, i.e., H is a vector!
+                temp0 = np.reshape(np.asarray(range(0, m * n)), [n, m]).T
+                temp = temp0[:, H]
+                self.pattern = H
+                temp = temp.T.reshape(-1)  # Column Vectorization
+                self.H = csr_matrix((np.ones(temp.shape), (range(0, len(temp)), temp)), shape=(len(temp), int(m * n)))
+            else:
+                # Takes the input H as the sampling matrix
+                self.H = H
         else:
+            # H here is a given subsampling value
+            # Load a pre-determinated random pattern
             Nsub = int(np.round(m * (H)))
             # iava = np.random.permutation(m)
             iava = np.squeeze(loadmat('data/iava.mat') ['iava']) - 1
             self.cort = np.sort(iava [Nsub:])
             iava = np.sort(iava [0:Nsub])
-            temp = np.asarray(range(0, int(m * n), m))
-            temp = np.tile(temp, (len(iava), 1))
-            temp = np.expand_dims(iava.T, -1) + temp
-            temp = np.squeeze(temp.T.reshape(-1))
+            self.pattern = iava
+            temp0 = np.reshape(np.asarray(range(0, m * n)), [n, m]).T
+            temp = temp0[:, iava]
+            temp = temp.T.reshape(-1)  # Column Vectorization
             self.H = csr_matrix((np.ones(temp.shape), (range(0, len(temp)), temp)), shape=(len(temp), int(m * n)))
+
 
         # ---------- Load or create the basis function  ---------
 
@@ -116,9 +156,13 @@ class Algorithms:
         :return: measures Y
         '''
 
-        return self.H * np.squeeze(self.x.reshape(-1))
+        return self.H * np.squeeze(self.x.T.reshape(-1))
 
-    def FISTA(self, lamnda, mu, max_itr):
+
+    # ---------------------------------------------FISTA----------------------------------------
+
+    def FISTA(self, lmb, mu, max_itr):
+
 
         y = self.measurements()
 
@@ -140,7 +184,7 @@ class Algorithms:
             z = s_old - mu * (grad)
 
             # proximal
-            x = soft_threshold(z, lamnda)
+            x = soft_threshold(z, lmb)
 
             q = 0.5 * (1 + np.sqrt(1 + 4 * (q_old ** 2)))
             s = x + ((q_old - 1) / (q)) * (x - x_old)
@@ -157,9 +201,8 @@ class Algorithms:
 
         return self.operator_inv(s), hist
 
-
     # ---------------------------------------------GAP----------------------------------------
-    def GAP(self, lamnda, max_itr): # 311021
+    def GAP(self, lmb, max_itr):
 
         y = self.measurements()
 
@@ -183,7 +226,7 @@ class Algorithms:
             z = x - grad
 
             # proximal
-            x = soft_threshold(z, lamnda)
+            x = soft_threshold(z, lmb)
             itr = itr + 1
 
             residualx = np.linalg.norm(x - x_old) / np.linalg.norm(x)
@@ -197,8 +240,8 @@ class Algorithms:
 
         return self.operator_inv(x), hist
 
-    #---------------------------------------------
-    def TwIST(self, lamnda, alpha, beta, max_itr):
+    #----------------TWIST----------------------------
+    def TwIST(self, lmb, alpha, beta, max_itr):
 
         y = self.measurements()
 
@@ -223,7 +266,7 @@ class Algorithms:
             z = x - grad
 
             # proximal
-            s = soft_threshold(z, lamnda)
+            s = soft_threshold(z, lmb)
 
             # Actualizacion
             temp = (1 - alpha)* x_old + (alpha - beta)* x + beta * s
@@ -242,67 +285,3 @@ class Algorithms:
             print(itr, '\t Error:', format(hist[itr, 0], ".4e"), '\t PSNR:', format(hist[itr, 1],".3f"), 'dB \n')
 
         return self.operator_inv(x), hist
-
-    def ADMM(self, rho, gamma, lamnda, max_itr):
-
-
-        s=0
-
-
-        y = self.measurements()
-
-        print('---------ADMM method---------- \n')
-
-        hist = np.zeros((max_itr + 1, 2))
-        dim = self.x.shape
-        x = np.zeros(dim)
-
-        begin_time = time.time()
-
-        residualx = 1
-        tol = 1e-3
-
-        v = x.ravel()
-        u = x.ravel()
-
-        print('itr \t ||x-xold|| \t PSNR \n')
-        itr = 0
-
-        HtY = self.A.transpose(y)
-
-        while (itr < max_itr ): #& residualx <= tol):
-            x_old = x
-
-            # F-update
-            temp = HtY.ravel() + np.sqrt(rho)*(v-u)
-            x = ln.cgs(self.A, temp, x0=None, tol=1e-05, maxiter=20)
-
-            # Proximal
-            vtilde = x + u
-            v = soft_threshold(vtilde, lamnda/rho)
-
-            # Update langrangian multiplier
-            u = u + (x - v)
-
-            # update rho
-            rho = rho * gamma
-
-            itr+=1
-
-            residualx = np.linalg.norm(x - x_old)  / np.linalg.norm(x)
-
-            x = np.reshape(x, [self.m, self.n])
-            psnr_val = PSNR(self.operator_inv(x), x_old)
-            hist[itr, 0] = residualx
-            hist[itr, 1] = psnr_val
-
-            if (itr + 1) % 5 == 0:
-                # mse = np.mean(np.sum((y-A(v,Phi))**2,axis=(0,1)))
-                end_time = time.time()
-                # Error = %2.2f,
-                print("ADMM-TV: Iteration %3d,  Error = %2.2f, PSNR = %2.2f dB," 
-                      " time = %3.1fs."
-                      % (itr + 1, residualx, psnr_val, end_time - begin_time))
-                      #% (ni + 1, psnr(v, X_ori), end_time - begin_time))
-
-        return self.operator_inv(v),hist
--- Algorithms/main.py
+++ Algorithms/main.py
@@ -5,29 +5,37 @@ from Function import *
 import scipy
 #----------------- --------------------
 x = np.load('data/data.npy')
-maxiter = 100
+x = x.T
+maxiter = 1000
 
-# Select the Algorithm
-case = 1 # opt: 0,1,2
-#----------------- FISTA --------------------
-if case == 0:
-    H = 0.75
-    Alg = Algorithms(x,H , 'DCT2D', 'IDCT2D')
 
+'''
+---------------  SAMPLING --------------------
+'''
+sr_rand = 0.5 # 1-compression
+y_rand, pattern_rand, pattern_index= random_sampling(x,sr_rand)
+H = pattern_index
+
+'''
+---------------- RECOVERY ALGORITHM -----------------
+Select the Algorithm: FISTA , GAP , TWIST , ADMM
+'''
+case = 'FISTA'
+#----------------- FISTA ------------------------------
+if case == 'FISTA':
+    Alg = Algorithms(x, H , 'DCT2D', 'IDCT2D')
     tau = 0.1
     mu = 0.3
-    x_result, hist = Alg.FISTA(tau, mu,maxiter)
+    x_result, hist = Alg.FISTA(tau, mu, maxiter)
 
 # ------------------GAP--------------
-if case == 1:
-    H = 0.75
+if case == 'GAP':
     Alg = Algorithms(x,H, 'DCT2D', 'IDCT2D')
     tau = 1e-0
     x_result, hist = Alg.GAP(tau, maxiter) # inputs: tau, maxiter
 
 # ------------------TwIST--------------
-if case == 2:
-    H = 0.75
+if case == 'TWIST':
     Alg = Algorithms(x,H, 'DCT2D', 'IDCT2D')
     # default parameters
     alpha = 1.2
@@ -37,8 +45,7 @@ if case == 2:
     x_result, hist = Alg.TwIST(tau, alpha, beta, maxiter)
 
 # --------------- ADMM -----------------
-if case == 3:
-    H = 0.75
+if case == 'ADMM':
     Alg = Algorithms(x,H, 'DCT2D', 'IDCT2D')
     # default parameters
     # step_size = 1e-2
@@ -46,25 +53,39 @@ if case == 3:
     # eta = 1e-1
     rho = 0.5
     gamma = 1
-    lamnda = 0.0078
+    lmb = 0.0078
 
-    x_result, hist = Alg.ADMM(rho, gamma, lamnda, maxiter)
+    x_result, hist = Alg.ADMM(rho, gamma, lmb, maxiter)
 # --------------------------------
 
 #-------------- Visualization ----------------
+
+temp = np.asarray(range(0, pattern_rand.shape[0]))
+pattern_rand_b2 = np.asarray(pattern_rand, dtype=bool) == 0
+H_elim = temp[pattern_rand_b2]
+
+
 x = Alg.x
-cort = Alg.cort
 
-plt.subplot(2, 2, 1), plt.imshow(x.T, cmap='gray', aspect='auto')
-plt.title('Real data')
-y_gor = x.copy()
-y_gor[cort,:]= 0
-plt.subplot(2, 2, 2), plt.imshow(y_gor.T, cmap='gray', aspect='auto')
-plt.title('Input')
-plt.subplot(2, 2, 3), plt.imshow(x_result.T, cmap='gray', aspect='auto')
-plt.title('Reconstruction')
-plt.subplot(2, 2, 4), plt.plot(x [cort [1], :], 'r', label='Reference')
-plt.plot(x_result [cort [1], :], 'b', label='Recovered')
-plt.legend()
+fig, axs = plt.subplots(2, 2,dpi=150)
+fig.suptitle('Results from the ' + case + ' Algorithm')
+
+axs[0, 0].imshow(x, cmap='seismic', aspect='auto')
+axs[0, 0].set_title('Real data')
+
+axs[1, 0].imshow(y_rand, cmap='seismic', aspect='auto')
+axs[1, 0].set_title('Incomplete')
+
+# axs[1, 0].sharex(axs[0, 0])
+
+axs[0, 1].imshow(x_result, cmap='seismic', aspect='auto')
+axs[0, 1].set_title('Reconstructed (PSNR:' + str("{:.1f})".format(hist[maxiter,1])))
+
+index = 5
+axs[1, 1].plot(x [H_elim[index], :], 'r', label='Reference')
+axs[1, 1].plot(x_result [H_elim[index], :], 'b', label='Recovered')
+axs[1, 1].legend(loc='best')
+plt.title('Trace ' + str("{:.0f}".format(H_elim[index])))
+
+fig.tight_layout()
 plt.show()
-
