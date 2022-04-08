2cd745e297711a8a4f20ff211614e8053d7dddbc
Carlos Hinojosa <carlos.hinojosa93@gmail.com>
2022-04-07 23:12:56 -0500

    adding plot func to tensor_test

--- Algorithms/tensor_test.py
+++ Algorithms/tensor_test.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from Function import *
 import time
 import cv2
+import matplotlib
+from skimage.metrics import structural_similarity as ssim
 
 
 def ten2mat(tensor, mode):
@@ -84,13 +86,72 @@ def GLTC_Geman(dense_tensor, sparse_tensor, alpha, beta, rho, theta, maxiter):
     return tensor_hat
 
 
+def plot_results(x, x_result, pattern_rand, case):
+    # -------------- Visualization ----------------
+    y_rand = x[:, int(x.shape[1] / 2)].copy()
+    y_rand[:, pattern_rand == 0] = 0
+
+    plt.imshow(y_rand, cmap='seismic', aspect='auto')
+    plt.title("Removed shots")
+    plt.xlabel("Shots")
+    plt.ylabel("Time")
+    plt.show()
+
+    # x = Alg.x
+    matplotlib.rcParams.update({'font.size': 8})
+    fig, axs = plt.subplots(2, 4, dpi=250)
+    fig.suptitle('Results from the ' + str(case) + ' Algorithm')
+    rem_shots = np.arange(len(pattern_rand))
+    rem_shots = rem_shots[pattern_rand == 0]
+    psnr_vec = []
+    for s in rem_shots:
+        psnr_vec.append(PSNR(x[..., s], x_result[..., s]))
+    idxs = (-np.array(psnr_vec)).argsort()
+    rem_shots = rem_shots[idxs]
+    axs[0, 0].imshow(x[..., rem_shots[0]], cmap='seismic', aspect='auto')
+    axs[0, 0].set_title(f'Reference, shot {rem_shots[0]}')
+
+    axs[1, 0].imshow(x[..., rem_shots[1]], cmap='seismic', aspect='auto')
+    axs[1, 0].set_title(f'Reference, shot {rem_shots[1]}')
+
+    metric = PSNR(x[..., rem_shots[0]], x_result[..., rem_shots[0]])
+    metric_ssim = ssim(x[..., rem_shots[0]], x_result[..., rem_shots[0]])
+    axs[0, 1].imshow(x_result[..., rem_shots[0]], cmap='seismic', aspect='auto')
+    axs[0, 1].set_title(f'Reconstructed shot {rem_shots[0]}, \n PSNR: {metric:0.2f} dB, \n SSIM:{metric_ssim:0.2f}')
+
+    metric = PSNR(x[..., rem_shots[1]], x_result[..., rem_shots[1]])
+    metric_ssim = ssim(x[..., rem_shots[1]], x_result[..., rem_shots[1]])
+    axs[1, 1].imshow(x_result[..., rem_shots[1]], cmap='seismic', aspect='auto')
+    axs[1, 1].set_title(f'Reconstructed shot {rem_shots[1]}, \n PSNR: {metric:0.2f} dB, \n SSIM:{metric_ssim:0.2f}')
+
+    # ====
+    axs[0, 2].imshow(x[..., rem_shots[2]], cmap='seismic', aspect='auto')
+    axs[0, 2].set_title(f'Reference, shot {rem_shots[2]}')
+
+    axs[1, 2].imshow(x[..., rem_shots[3]], cmap='seismic', aspect='auto')
+    axs[1, 2].set_title(f'Reference, shot {rem_shots[3]}')
+
+    metric = PSNR(x[..., rem_shots[2]], x_result[..., rem_shots[2]])
+    metric_ssim = ssim(x[..., rem_shots[2]], x_result[..., rem_shots[2]])
+    axs[0, 3].imshow(x_result[..., rem_shots[2]], cmap='seismic', aspect='auto')
+    axs[0, 3].set_title(f'Reconstructed shot {rem_shots[2]}, \n PSNR: {metric:0.2f} dB, \n SSIM:{metric_ssim:0.2f}')
+
+    metric = PSNR(x[..., rem_shots[3]], x_result[..., rem_shots[3]])
+    metric_ssim = ssim(x[..., rem_shots[3]], x_result[..., rem_shots[3]])
+    axs[1, 3].imshow(x_result[..., rem_shots[3]], cmap='seismic', aspect='auto')
+    axs[1, 3].set_title(f'Reconstructed shot {rem_shots[3]}, \n PSNR: {metric:0.2f} dB, \n SSIM:{metric_ssim:0.2f}')
+
+    fig.tight_layout()
+    plt.show()
+
+
 if __name__ == '__main__':
     theta = 2
     alpha = 1000
     rho = 0.01
     beta = 0.1 * rho
     maxiter = 1000
-    data_name = 'cube4.npy'
+    data_name = 'syn3D_cross-spread2.npy'
 
     x = np.load('../data/' + data_name)
 
@@ -101,7 +162,7 @@ if __name__ == '__main__':
     '''
     ---------------  SAMPLING --------------------
     '''
-    sr_rand = 0.4  # 1-compression
+    sr_rand = 0.5  # 1-compression
     y_rand, pattern_rand, pattern_index = random_sampling(x[:, int(x.shape[1] / 2), :], sr_rand)
     H = pattern_index
     y = x.copy()
@@ -125,12 +186,13 @@ if __name__ == '__main__':
     aux_s = np.zeros(x.shape[-1])
     for i in range(1, x.shape[-1] - 1):
         tmp = cv2.inpaint(x[:, :, [i - 1, i, i + 1]], mask[..., 0], t_m+1, flags=paint_method)
-        output[..., [i - 1, i, i + 1]] += tmp
-        aux_s[[i - 1, i, i + 1]] += 1
-    output /= aux_s
+        output[..., [i - 1, i, i + 1]] = tmp
+        # aux_s[[i - 1, i, i + 1]] += 1
+    # output /= aux_s
     output = np.transpose(output, [0, 2, 1])
     x = np.transpose(x, [0, 2, 1])
-    PSNR(x, output)
+    print(PSNR(x, output))
+    plot_results(x, output, pattern_rand, 'Fast Marching (Inpainting)')
     # x = x[:, :3, :]
     # y = y[:, :3, :]
     # y = np.reshape(y, [x.shape[0] * x.shape[1], x.shape[2]])
